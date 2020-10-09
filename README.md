66eabcbb2f85fb444b2ffd487b1560b55d074831
Elijah Baraza <ebaraza10@gmail.com>
2020-10-09 16:18:39 +0300

    Creating wallets here

---
+++ README.md
@@ -0,0 +1,4 @@
+packages:
+nodeman: application server/for running the application
+jest: for tests
+express : for APIs
---
+++ dev-test.js
@@ -0,0 +1,8 @@
+const Blockchain = require('./blockchain');
+
+
+const bc = new Blockchain();
+
+for(let i=0;i<10;i++){
+    console.log(bc.addBlock(`foo {$i}`).toString());
+}
